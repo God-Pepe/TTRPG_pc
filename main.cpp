0ee8baedc51ca46f53c7f04ffaf2f14ff60137f2
Boris <GodPepe55@yandex.ru>
2025-07-30 22:48:06 +0500

    fightinmain

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ public:
     Enemy fighter;
 
     void startGame() {
+
         hero = *(new Player());
         hero.take_hp();
         hero.take_intelect();
@@ -26,6 +27,13 @@ public:
         cout << "Ваша сила: " << hero.get_strength()  << '\n';
         cout << "Ваша ловкость: " << hero.get_dexterity() << '\n';
         cout << "Ваша метскость: " << hero.get_accuracy() << '\n';
+        cout << " " << '\n';
+
+        set_hpCr(0);
+        set_intelectCr(0);
+        set_strengthCr(0);
+        set_dexterityCr(0);
+        set_accuracyCr(0);
     };
 
     int endGame() {
@@ -49,11 +57,48 @@ public:
         fighter.attack(fighter.bagpackE[0], hero);
     };
 
-    void actionPlayer(Player hero, int act) {
+    void actionPlayer(Player hero) {
+        int act;
         cout << "выбирите действие: [1] -- атака" << '\n';
         cin >> act;
         if (act == 1) {
             hero.attack(hero.bagpack[0], fighter);
+        }
+    };
+
+    void battle() {
+        creatEnemy();
+
+        cout << "На вас напали!" << '\n';
+
+        while(true) {
+            actionPlayer(hero);
+
+            if (fighter.hpd <= 0) {
+                fighter.die();
+                cout << "Вы победили!" << '\n';
+                break;
+            }
+
+            else if (fighter.hpd != 0) {
+                cout << "Ход врага!" << '\n';
+                continue;
+            }
+
+            actionEnemy(fighter);
+
+            if (hero.hpd <= 0) {
+                cout << "Вы погибли в схватке..." << '\n';
+                hero.die();
+                PlayerForm(hero);
+                break;
+            }
+
+            else if (hero.hpd != 0) {
+                cout << "Ваш ход! " << '\n';
+                continue;
+            }
+
         }
     };
 }; 
@@ -63,6 +108,8 @@ int main() {
     scene land = *(new scene());
 
     land.startGame();
+
+    land.battle();
 
     return 0;
 }
